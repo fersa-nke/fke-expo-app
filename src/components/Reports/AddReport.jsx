f22219a0d9698044fb0ff51d668292f6e7ae7737
Manoj Kumar <hashmemanoj@gmail.com>
2023-05-21 19:06:46 +0530

    Add Report UI Design

--- src/components/Reports/AddReport.jsx
+++ src/components/Reports/AddReport.jsx
@@ -11,6 +11,8 @@ import Button from "../../shared/Button";
 import { useSelector, useDispatch } from "react-redux";
 import { saveJobReport } from "../../redux/Reports/ReportsAction";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import Select from "../../shared/Select";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const AddReport = () => {
   const navigation = useNavigation();
@@ -39,11 +41,102 @@ const AddReport = () => {
 
   return (
     <ScrollView style={{ backgroundColor: theme.bgWhite }}>
-      <View style={GBStyles.container}>
-        <Input placeholder="Report Name" labelName="Report Name" handleChangeText={setReportName} style={{ marginBottom: 20 }} />
-        <View style={{ marginBottom: 24 }}>
+      <KeyboardAwareScrollView style={GBStyles.container}>
+        <View style={{backgroundColor: '#f4f4f4', padding: 12, marginBottom: 20, borderRadius: 8}}>
+        <Text style={{ fontSize: 16}}>Job - <Text style={{fontWeight: '700'}}>J1234</Text></Text>
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Report Id" mand labelName="Report Id" handleChangeText={setReportName} value={'RM-458458468854-RP'} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
         <Input
-          labelName="Date"
+          labelName="Report Date"
+          placeholder="DD/MM/YYYY"
+          appendIconName="Calendar"
+          appendIconColor={theme.textBlue}
+          appendIconSize={16}
+          value={startDate.toLocaleDateString()}
+          handlePress={() => setShow(true)}
+          mand
+        />
+        {show && (
+          <DateTimePicker
+            value={startDate}
+            mode="date"
+            onChange={onChange}
+            display={Platform.OS === "ios" ? "spinner" : "calendar"}
+          />
+        )}
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Name" labelName="Name" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Serial No" labelName="Serial No" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Select
+            selectedValue={''}
+            disabled={false}
+            onChange={() => console.log('Selected')}
+            placeholder="Select NOK Bearing"
+            label="NOK Bearing"
+            modalTitle="Select NOK Bearing"
+            // items={{ Id: "Id", Name: "Name" }}
+            modalObj={{ id: "Id", name: "Name" }}
+          />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Shaft NDE Min" labelName="Shaft NDE Min" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Shaft NDE Max" labelName="Shaft NDE Max" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Housing NDE Min" labelName="Housing NDE Min" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Housing NDE Max" labelName="Housing NDE Max" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Shaft DE Min" labelName="Shaft DE Min" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Shaft DE Max" labelName="Shaft DE Max" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Housing DE Min" labelName="Housing DE Min" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Housing DE Max" labelName="Housing DE Max" keyboardType="numeric" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Select
+            selectedValue={''}
+            disabled={false}
+            onChange={() => console.log('Selected')}
+            placeholder="Select Lubrication Type"
+            label="Lubrication Type"
+            modalTitle="Select Lubrication Type"
+            // items={{ Id: "Id", Name: "Name" }}
+            modalObj={{ id: "Id", name: "Name" }}
+          />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Select
+            selectedValue={''}
+            disabled={false}
+            onChange={() => console.log('Selected')}
+            placeholder="Select Lubrication Grade"
+            label="Lubrication Grade"
+            modalTitle="Select Lubrication Grade"
+            // items={{ Id: "Id", Name: "Name" }}
+            modalObj={{ id: "Id", name: "Name" }}
+          />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+        <Input
+          labelName="Last Lubrication"
           placeholder="DD/MM/YYYY"
           appendIconName="Calendar"
           appendIconColor={theme.textBlue}
@@ -60,6 +153,20 @@ const AddReport = () => {
           />
         )}
         </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Insulate Resistance" labelName="Insulate Resistance" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Input placeholder="Voltage Tested" labelName="Voltage Tested" handleChangeText={setReportName} />
+        </View>
+        <View style={{ marginBottom: 24 }}>
+          <Input placeholder="Comments" labelName="Comments" multiline={true} handleChangeText={setReportName} />
+        </View>
+
+        
+
+
+
         <Row>
           <Button
             text="Save"
@@ -73,7 +180,7 @@ const AddReport = () => {
             onPress={() => navigation.navigate("ReportView")}
           />
         </Row>
-      </View>
+      </KeyboardAwareScrollView>
     </ScrollView>
   );
 };
